5b70f2f25b241eea8c3425465569b0b14c616d9a
nhat.than.zy <nhat.than.zy@renesas.com>
2026-05-19 08:59:20 +0700

    Add RA test project and .gitignore file
    Change-Id: I84a106f754df3649216d18260163922c1b8a1883

---
+++ MoreSoftwareProjects/risv/HardwareDebug/src/risv.d
@@ -0,0 +1,3 @@
+src/risv.o: ..\src\risv.c \
+  C:\Users\a5118811\git\e2studio\MoreSoftwareProjects\risv\generate\iodefine.h
+C:\Users\a5118811\git\e2studio\MoreSoftwareProjects\risv\generate\iodefine.h:
